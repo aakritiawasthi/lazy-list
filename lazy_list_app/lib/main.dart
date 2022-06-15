e6e1c4112301b52f19181c32d0d8e0a5f24798a9
Aakriti Awasthi <aakritiawasthi1343@gmail.com>
2022-06-15 22:46:45 +0530

    day2 completed

--- lazy_list_app/lib/main.dart
+++ lazy_list_app/lib/main.dart
@@ -1,3 +1,8 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -11,18 +16,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'
-          ),
-          ),
-          body: const Center(
-            child: RandomWords(),
-          ),
-           ),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('Startup Name Generator'
+      //     ),
+      //     ),
+      //     body: const Center(
+      //       child: RandomWords(),
+      //     ),
+      //      ),
+      home: RandomWords(),
     );
   }
 }
@@ -30,11 +36,25 @@ class MyApp extends StatelessWidget {
 
 class _RandomWordsState extends State<RandomWords> {
    final _suggestions = <WordPair>[];
+   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
- 
+  
      @override 
     Widget build(BuildContext context) {
-      return ListView.builder(
+      void _pushSaved(){}
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+            onPressed: _pushSaved, 
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+        ),
+     
+      body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i){
           if (i.isOdd) return const Divider();
@@ -43,14 +63,32 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
+          final alreadySaved = _saved.contains(_suggestions[index]);
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
           ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+          ),
+          onTap: (){
+            setState(() {
+              if (alreadySaved){
+                _saved.remove(_suggestions[index]);
+              } else {
+                _saved.add(_suggestions[index]);
+              }
+            });
+          },
           );
+           
         }
-        );
+        ),
+         );
+
     //final wordPair = WordPair.random();
     //return Text(wordPair.asPascalCase);
   }
